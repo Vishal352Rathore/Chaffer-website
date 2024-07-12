b57a64c1b03de35a9561368668883101114328bc
Bandana Satpathy <bandna.quicklyplatforms@gmail.com>
2024-07-12 18:05:08 +0530

    12 july changes

--- src/components/Homeslide.jsx
+++ src/components/Homeslide.jsx
@@ -7,9 +7,9 @@ import homeslide3 from "../cab_images/backgroundslide3.jpg";
 const Homeslide = () => {
   return (
 <div className='home_slide_carousel'>
-    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-     <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+     <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
